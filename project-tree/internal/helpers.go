58b9bcbfae495fa2d19b6d813ca87329a80f4ea0
zhaolushka <kairakbaiinabat09@gmail.com>
2025-08-01 02:19:40 +0500

    Initial commit

---
+++ project-tree/internal/helpers.go
@@ -0,0 +1,22 @@
+$ ls -laR project-tree
+total 0
+drwxr-xr-x  4 student  student  128 May 30 16:21 .
+drwxr-xr-x  3 student  student   96 May 30 16:23 ..
+drwxr-xr-x  3 student  student   96 May 30 16:21 config
+drwxr-xr-x  7 student  student  224 May 30 16:21 internal
+
+project-tree/config:
+total 0
+drwxr-xr-x  3 student  student   96 May 30 16:21 .
+drwxr-xr-x  4 student  student  128 May 30 16:21 ..
+-rw-------  1 student  student    0 May 30 16:21 settings.conf
+
+project-tree/internal:
+total 0
+drwxr-xr-x  7 student  student  224 May 30 16:21 .
+drwxr-xr-x  4 student  student  128 May 30 16:21 ..
+-rwxr-x---  1 student  student    0 May 30 16:21 app.go
+-rwxr-x---  1 student  student    0 May 30 16:21 app_test.go
+-rwxr-x---  1 student  student    0 May 30 16:21 helpers.go
+-rwxr-x---  1 student  student    0 May 30 16:21 helpers_test.go
+lrwxr-xr-x  1 student  student   23 May 30 16:21 settings.link -> ../config/settings.conf
